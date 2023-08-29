efaff7d857f7a8d4bb0dbd6884f9a468ce71b87c
Rick Lane <rlane@datawire.io>
2023-08-29 09:18:14 -0700

    Testing tracing with a plugin filter
    Signed-off-by: Rick Lane <rlane@datawire.io>

--- param-plugin.go
+++ param-plugin.go
@@ -3,31 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func PluginMain(w http.ResponseWriter, r *http.Request) {
-	keys, ok := r.URL.Query()["db"]
+	clientId := "0393c616-8b0d-4e5b-9dce-4667f1fcb609"
 
-	if !ok || len(keys[0]) < 1 {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
+	log.Printf("Setting x-client-trace-id to %s", clientId)
 
-	key := keys[0]
-	keyInt, err := strconv.Atoi(key)
-
-	if err != nil {
-		log.Printf("param is not an int \n")
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	if keyInt%2 == 0 {
-		w.Header().Set("X-DC", "Even")
-	} else {
-		w.Header().Set("X-DC", "Odd")
-	}
+	r.Header.Set("x-client-trace-id", clientId)
+	r.Header.Set("x-envoy-force-trace", "true")
 
 	w.WriteHeader(http.StatusOK)
 }
